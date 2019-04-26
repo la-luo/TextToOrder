d45c4ea0ed268e91a80cbb1f3143eb0e6e3888a5
La Luo <rola.uiuc@gmail.com>
2019-04-26 14:41:15 -0700

    get current user info

--- client/src/components/splash.js
+++ client/src/components/splash.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import {Redirect, Link} from 'react-router-dom';
+import { connect } from 'react-redux';
 
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.session
+  }};
 
 class Splash extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            currentUser: 'noUser',
 
         }
 
     }
 
+    componentDidMount(){
+        if(this.props.currentUser.id) {
+            this.setState({currentUser: this.props.currentUser.username});
+        }
+    }
+
 
     render() {
+        console.log(this.props.currentUser);
 
         return (
             <div>
-                <p>Hello</p>
+                
+                {this.state.currentUser==='noUser'? <div><Link to='/signup'>Signup</Link>
+                <Link to='/login'>Login</Link></div>: <div><p>Hello {this.state.currentUser}</p><p>logout</p></div>}
+                
             </div>
         )
     }
 
 }
-
-export default Splash;
+// do not need export default Splash
+export default connect(mapStateToProps, null)(Splash);
--- client/src/reducers/session_reducer.js
+++ client/src/reducers/session_reducer.js
@@ -2,20 +2,21 @@ import {
     RECEIVE_CURRENT_USER
   } from '../actions/session_actions';
   
-  const _nullUser = Object.freeze({
+  const nullUser = Object.freeze({
     id: null
   });
   
-  const sessionReducer = (state = _nullUser, action) => {
+  const sessionReducer = (state = nullUser, action) => {
     Object.freeze(state);
     switch(action.type) {
       case RECEIVE_CURRENT_USER:
       if (Object.values(action.payload).length === 0) {
-        return _nullUser
+        return nullUser
       } else {
         return {
           id: action.payload.id,
-          username: action.payload.username };
+          username: action.payload.username,
+          email: action.payload.email };
         }
       default:
         return state;
