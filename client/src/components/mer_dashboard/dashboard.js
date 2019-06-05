f5ca71d0933f252bb1196839f7fba84575aefdbb
La Luo <rola.uiuc@gmail.com>
2019-06-05 16:39:19 -0700

    merchant dashboard styling

--- client/src/components/mer_dashboard/dashboard.js
+++ client/src/components/mer_dashboard/dashboard.js
@@ -23,13 +23,45 @@ class merDashboard extends React.Component {
     render() {
 
         return (
-            <div>
-                <h3>Your Restaurant Information</h3>
+            <div className="home">
+            <div className="container-fluid display-table">
+                    <div className="col-md-2 col-sm-1 hidden-xs display-table-cell v-align box" id="navigation">
+                        <div className="logo">
+                            <a href="home.html">Text To Order</a>
+                        </div>
+                        <div className="navi">
+                            <ul>
+                                <li className="active"><a href="#"><i className="fa fa-home" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Home</span></a></li>
+                                <li><a href="#"><i className="fa fa-list-alt" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Menu</span></a></li>
+                                <li><a href="#"><i className="fa fa-calendar" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Orders</span></a></li>
+                                <li><a href="#"><i className="fa fa-cog" aria-hidden="true"></i><span className="hidden-xs hidden-sm">Setting</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-10 col-sm-11 display-table-cell v-align">
+                        <div className="row">
+                            <header>
+                                <div className="col-md-7">
+                                    <nav className="navbar-default pull-left">
+                                        <div className="navbar-header">
+                                        <a>Hello</a>
+                                        </div>
+                                    </nav>
+                                </div>
+                                <div className="col-md-5">
+                                    <div className="header-rightside"></div>
+                                </div>
+                            </header>
+                        </div>
+                        <div className="user-dashboard">
 
-                <h3>Your Menu</h3>
+                        </div>
+                    </div>
 
-                <h3>Order History</h3>
             </div>
+
+
+        </div>
         )
     }
 
