003084ba697bb541ee38f90b292972b82195a686
La Luo <laluo.work@gmail.com>
2020-12-29 19:29:06 -0500

    sms and homepage

--- backend/api/routes/sms.js
+++ backend/api/routes/sms.js
@@ -5,27 +5,30 @@ const http = require('http');
 const keys = require('../../config/keys');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
+const Merchant = require('../models/Merchant');
 
 router.use(session({secret: keys.sessionSecretKey }));
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/sms', (req, res) => {
+router.post('/sms/:phoneNumber', (req, res) => {
+
     const twiml = new MessagingResponse();
-
-    // if (req.body.Body == 'hello') {
-    //   twiml.message('Hi!');
-    // } else if (req.body.Body == 'bye') {
-    //   twiml.message('Goodbye');
-    // } else {
-    //   twiml.message(
-    //     'No Body param match, Twilio sends this in the request to your server.'
-    //   );
-    // }
-
-    twiml.message("Hi, thank you for visiting xxx. Please text us 'd' or 'p' to inform us whether it is for delivery or pickup.");
+    Merchant.findOne({ phone: req.params.phoneNumber }).then(merchant => {
+      
+      if (merchant) {
+        twiml.message(`Hi, thank you for visiting ${merchant.storename}. Please text us 'd' or 'p' to inform us whether it is for delivery or pickup.`);
   
-    res.writeHead(200, { 'Content-Type': 'text/xml' });
-    res.end(twiml.toString());
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        res.end(twiml.toString());
+
+      } else {
+        twiml.message('Merchant Not Found');
+  
+        res.writeHead(500, { 'Content-Type': 'text/xml' });
+        res.end(twiml.toString());
+      }
+    })
+
   });
   
 
--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -29,44 +29,59 @@ class Splash extends React.Component {
         //console.log(this.props.currentUser);
 
         return (
-            <div>
-<nav className="navbar navbar-default navbar-expand-lg navbar-light">
-	<div className="navbar-header d-flex col">
-		<a className="navbar-brand" href="#">Text To Order</a>  		
-		<button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle navbar-toggler ml-auto">
-			<span className="navbar-toggler-icon"></span>
-			<span className="icon-bar"></span>
-			<span className="icon-bar"></span>
-			<span className="icon-bar"></span>
-		</button>
-	</div>
-	<div id="navbarCollapse" className="collapse navbar-collapse justify-content-start">
-		{localStorage.getItem('jwtToken ') === null ? 
-		<ul className="nav navbar-nav navbar-right ml-auto">
-         <li className="nav-item"><Link className="nav-link" to='/login'>Sign In</Link></li>
-		 <li className="nav-item"><Link className="btn btn-primary get-started-btn mt-1 mb-1" to='/signup'>Sign Up</Link></li>
-		</ul>
-		 : 
-		 <ul className="nav navbar-nav navbar-right ml-auto">
-		 <li className="nav-item"><Link className="nav-link" to='/signup'>Sign Up</Link></li>
-		 <li className="nav-item"><Link className="btn btn-primary" to='/#'>Logout</Link></li>
-		 </ul>}		
-	</div>
-</nav>
-<section>
-				<div className="intro">
-                  <h1>Text To Order</h1>
-                </div>
-                <div className="splash">
-                </div>
-</section>
-
-
-			<footer>
-				<a href="/merchants/signup">Be a Partner Restaurant</a>
-			</footer>
-
-	</div>
+        <div>
+        <nav className="navbar navbar-default navbar-expand-lg navbar-light">
+            <div className="navbar-header d-flex col">
+                <a className="navbar-brand" href="#">Text To Order</a>  		
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle navbar-toggler ml-auto">
+                <span className="navbar-toggler-icon"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                </button>
+            </div>
+            <div id="navbarCollapse" className="collapse navbar-collapse justify-content-start">
+                {localStorage.getItem('jwtToken ') === null ? 
+                <ul className="nav navbar-nav navbar-right ml-auto">
+                    <li className="nav-item">
+                    <Link className="nav-link" to='/login'>
+                    Sign In</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="btn btn-primary get-started-btn mt-1 mb-1" to='/signup'>
+                    Sign Up</Link>
+                    </li>
+                </ul>
+                : 
+                <ul className="nav navbar-nav navbar-right ml-auto">
+                    <li className="nav-item">
+                    <Link className="nav-link" to='/signup'>
+                    Sign Up</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="btn btn-primary" to='/#'>
+                    Logout</Link>
+                    </li>
+                </ul>
+                }		
+            </div>
+        </nav>
+        <section>
+            <div className="intro">
+                <h1>Text To Order</h1>
+            </div>
+            <div className="splash">
+            </div>
+        </section>
+        <section>
+            <div className="find-business">
+                <h2>Find restaurants, cafes, shops...</h2>
+            </div>
+        </section>
+        <footer>
+            <a href="/merchants/signup">Become a Partner Restaurant</a>
+        </footer>
+        </div>
         )
     }
 
