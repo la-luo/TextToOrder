66c408c2da0acdcb3fc8d5c4f3ddbce3079584b2
La Luo <laluo.work@gmail.com>
2021-01-02 18:34:25 -0500

    prepare for deploy

---
+++ client/build/precache-manifest.d87ec383169539835f1b9f319520f827.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ab6b3a4ba92604a8106",
+    "url": "/static/css/main.18ce69f4.chunk.css"
+  },
+  {
+    "revision": "6ab6b3a4ba92604a8106",
+    "url": "/static/js/main.ee63a3fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "76f42c25f7ce1480cae3",
+    "url": "/static/js/2.263ac941.chunk.js"
+  },
+  {
+    "revision": "986f3dde57934d4c95bcaca0c1d0f5d9",
+    "url": "/static/media/fast-food.986f3dde.jpg"
+  },
+  {
+    "revision": "620cae9b824c0477728b074fab38349b",
+    "url": "/static/media/asian-food.620cae9b.jpg"
+  },
+  {
+    "revision": "c04319c230d6578baed1867244f51e4f",
+    "url": "/static/media/grocery.c04319c2.jpg"
+  },
+  {
+    "revision": "68381f87e65c0d6cb7166e5fea0574ad",
+    "url": "/static/media/texting.68381f87.gif"
+  },
+  {
+    "revision": "a50dbd8391609cfde2703ac92215cc24",
+    "url": "/static/media/cooking.a50dbd83.gif"
+  },
+  {
+    "revision": "d7347bcfb993e4d43b6c05a3aa88954f",
+    "url": "/index.html"
+  }
+];
